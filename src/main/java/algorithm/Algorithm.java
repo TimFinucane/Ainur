d79cf093f9d29b01e45a561903f33529c610f04b
Nathan Cairns <nathancairns777@gmail.com>
2018-07-28 15:12:41 +1200

    Add method stubs for Algorithm classes

--- src/main/java/algorithm/Algorithm.java
+++ src/main/java/algorithm/Algorithm.java
@@ -1,4 +1,23 @@
 package algorithm;
 
-public class Algorithm {
+import common.Graph;
+import common.Schedule;
+
+public abstract class Algorithm {
+    protected int _processors;
+    protected boolean _multithreaded;
+
+    public Algorithm(int processors, boolean multithreaded) {
+        _processors = processors;
+        _multithreaded = multithreaded;
+    }
+
+    public abstract void start(Graph graph);
+
+    // TODO Implement method
+    // This method is up for debate. May not be needed.
+    public boolean isComplete() { return false; }
+
+    // TODO Implement method
+    public Schedule getCurrentBest() { return null; }
 }
--- src/main/java/algorithm/DFSAlgorithm.java
+++ src/main/java/algorithm/DFSAlgorithm.java
@@ -1,4 +1,14 @@
 package algorithm;
 
+import common.Graph;
+
 public class DFSAlgorithm extends Algorithm {
+
+    public DFSAlgorithm(int processors, boolean multithreaded) {
+        super(processors, multithreaded);
+    }
+
+    // TODO Implement method
+    @Override
+    public void start(Graph graph) {}
 }
