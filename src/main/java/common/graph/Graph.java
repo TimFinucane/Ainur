89f2b6995fae3368febecf7f18ae35cfa739b0a9
Timothy Finucane <timfinucane@outlook.com>
2018-07-28 23:39:23 +1200

    Set up graph internals for efficient edge storage

--- src/main/java/common/graph/Graph.java
+++ src/main/java/common/graph/Graph.java
@@ -1,7 +1,5 @@
 package common.graph;
 
-import javafx.util.Pair;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,16 +9,39 @@ import java.util.Map;
  * Holds references to all nodes and edges in a Graph.
  */
 public class Graph {
+    private final List<Node>                _nodes;
 
-    private final List<Node> _nodes;
-    private final List<Edge> _edges;
+    // Storage of edges is relative to how nodes access them.
+    // N.B. The key is the node name as of now.
+    // TODO: Look into using the slightly faster ArrayList, where nodes have indices assigned to them.
+    private final Map<String, List<Edge>>   _incomingEdges = new HashMap<>();
+    private final Map<String, List<Edge>>   _outgoingEdges = new HashMap<>();
+
+    private final List<Node>                _entryPoints = new ArrayList<>();
 
     /**
      * Default constructor for a Graph object
      */
     public Graph(List<Node> nodes, List<Edge> edges) {
         _nodes = nodes;
-        _edges = edges;
+
+        // Construct the incoming and outgoing edges, relative to the nodes connected to them
+        for( Node node : nodes )
+        {
+            _outgoingEdges.put(node.getLabel(), new ArrayList<>());
+            _incomingEdges.put(node.getLabel(), new ArrayList<>());
+        }
+
+        for( Edge edge : edges )
+        {
+            _outgoingEdges.get(edge.getOriginNode().getLabel()).add(edge);
+            _incomingEdges.get(edge.getDestinationNode().getLabel()).add(edge);
+        }
+
+        // Find the entry points of the graph and cache them
+        for( Node node : _nodes )
+            if( _incomingEdges.get(node.getLabel()).isEmpty() )
+                _entryPoints.add(node);
     }
 
     /**
