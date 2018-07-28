cb763096fef19eebf238941c6a71c17131c358b9
Nathan Cairns <nathancairns777@gmail.com>
2018-07-28 15:26:30 +1200

    Add doc comments to DFSAlgorithm class

--- src/main/java/algorithm/DFSAlgorithm.java
+++ src/main/java/algorithm/DFSAlgorithm.java
@@ -2,12 +2,24 @@ package algorithm;
 
 import common.Graph;
 
+/**
+ * A DFS implementation of the Algorithm class.
+ */
 public class DFSAlgorithm extends Algorithm {
 
+    /**
+     * Constructor for DFSAlgorithm class.
+     * @param processors The number of processors.
+     * @param multithreaded Whether or not to use multithreading.
+     */
     public DFSAlgorithm(int processors, boolean multithreaded) {
         super(processors, multithreaded);
     }
 
+    /**
+     * Starts the scheduling algorithm. Uses DFS to calculate an optimal schedule.
+     * @param graph A graph object representing tasks needing to be scheduled.
+     */
     // TODO Implement method
     @Override
     public void start(Graph graph) {}
