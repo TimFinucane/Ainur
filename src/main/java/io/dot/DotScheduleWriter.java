aa9faac9da08ebf5dcb6cf5bb856bb5283cab60a
emipeanz <epea390@aucklanduni.ac.nz>
2018-07-29 12:23:10 +1200

    Add file formatters for when writing to a .dot file

--- src/main/java/io/dot/DotScheduleWriter.java
+++ src/main/java/io/dot/DotScheduleWriter.java
@@ -9,6 +9,11 @@ import java.io.OutputStream;
  * This class writes a schedule to a .dot file format.
  */
 public class DotScheduleWriter extends ScheduleWriter {
+
+    private final String NODE_DEPENDENCY_FORMAT = "%d -> %d\t [Weight=%d];";
+    private final String NODE_SINGLE_FORMAT = "%d\t [Weight=%d];";
+    private final String OUTPUT_FILE_NAME = "output.dot";
+    
     /**
      * Constructor for ScheduleWriter
      * @param os The output stream to write to.
