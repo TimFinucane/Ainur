35bdefa4a599b86d5d2de7824ab7c98feb050f82
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-07-29 12:19:41 +1200

    Add conversion from input stream to String

--- src/main/java/io/dot/DotGraphReader.java
+++ src/main/java/io/dot/DotGraphReader.java
@@ -2,8 +2,11 @@ package io.dot;
 
 import common.graph.Graph;
 import io.GraphReader;
+import sun.misc.IOUtils;
 
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 /**
  * Takes an InputStream in .dot format and converts it to a Graph object.
@@ -14,12 +17,26 @@ public class DotGraphReader extends GraphReader {
      * @param is The InputStream to read from.
      */
     public DotGraphReader(InputStream is) {
+
         super(is);
+
     }
 
     // TODO Implement method
     @Override
     public Graph read() {
+
+        String streamText = convertStreamToString(_is);
+
         return null;
+
+    }
+
+    private String convertStreamToString(InputStream inputStream) {
+
+        Scanner s = new Scanner(inputStream).useDelimiter("\\A");
+
+        return s.hasNext() ? s.next() : "";
+
     }
 }
