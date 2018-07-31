7a1ec5a66f734becdad7fe6c63e4d231d794a2f3
emipeanz <epea390@aucklanduni.ac.nz>
2018-07-31 17:04:06 +1200

    Add stub class that implements LowerBound to use in place of actual implementations

---
+++ src/main/java/algorithm/Heuristics/LowerBoundStub.java
@@ -0,0 +1,23 @@
+package algorithm.Heuristics;
+
+import common.graph.Graph;
+import common.graph.Node;
+import common.schedule.Schedule;
+
+import java.util.List;
+
+/**
+ * Class is a stub to use in place of actual LowerBound algorithms until official once have been made.
+ */
+public class LowerBoundStub implements LowerBound {
+
+
+    public int estimate(Graph graph, Schedule schedule, List<Node> nodesToVisit) {
+        return 0;
+    }
+
+
+    public int estimate(Graph graph, Schedule schedule) {
+        return 0;
+    }
+}
