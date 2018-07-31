d1a3c25a925391f5c6f02b39b9cc369ae032a5f3
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-08-01 11:51:43 +1200

    Lay out pseudo code

--- src/main/java/algorithm/heuristics/BustersPruner.java
+++ src/main/java/algorithm/heuristics/BustersPruner.java
@@ -17,6 +17,14 @@ public class BustersPruner implements Arborist {
      */
     //TODO - implementation
     public boolean prune(Graph graph, Schedule schedule, Task toBeAdded) {
+
+        // Extract getTotalTime() from schedule for finishing time of last current node, awaiting model changes.
+
+        // Awaiting changes for getEnd() on task to be added.
+
+        // Compare!!
+
         return false;
+
     }
 }
