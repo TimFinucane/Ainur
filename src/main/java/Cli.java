585c543c912fc7690276e5b508183b77c9f4c8ca
Nathan Cairns <nathancairns777@gmail.com>
2018-08-01 01:06:30 +1200

    Process all cli args

--- src/main/java/Cli.java
+++ src/main/java/Cli.java
@@ -1,23 +1,28 @@
 import common.Config;
 import org.apache.commons.cli.*;
 
+import java.util.List;
+
 public class Cli {
     private String[] _args;
     boolean _visualise;
     int _cores;
     String _outputFile;
     Options _options;
+    String _inputFile;
+    int _processors;
 
     public Cli(String[] args) {
         _args = args;
         _visualise = false;
         _cores = 1; // TODO default may change when this is implemented
+        _processors = 1;
+
+        // Apache Commons CLI: Definition Stage
         _options = establishOptions();
     }
 
     public void parse() throws ParseException {
-        // Apache Commons CLI: Definition Stage
-
         // Apache Commons CLI: Parsing Stage
         DefaultParser clParse = new DefaultParser();
         CommandLine cmdLine = clParse.parse(_options, _args);
@@ -26,21 +31,28 @@ public class Cli {
         if (cmdLine.hasOption("h") || cmdLine.getArgList().size() < 2) {
             displayUsage();
         }
+
+        List<String> argList = cmdLine.getArgList();
+        _inputFile = argList.get(0);
+        _processors = Integer.parseInt(argList.get(1));
+
         if (cmdLine.hasOption("p")) {
             _cores = Integer.parseInt(cmdLine.getOptionValue("p"));
             System.out.println("You instructed Ainur to be executed using " + _cores + " cores");
             System.out.println("Unfortunately this feature is yet to be implemented... stay tuned");
         }
         if (cmdLine.hasOption("v")) {
+            _visualise = true;
             System.out.println("You instructed Ainur to visualise the scheduling process!");
             System.out.println("Unfortunately this feature is yet to be implemented... stay tuned");
         }
         if (cmdLine.hasOption("o")) {
             _outputFile = cmdLine.getOptionValue("o");
             System.out.println("You instructed Ainur to output the schedule to a file called " + _outputFile);
+        } else {
+            _outputFile = _inputFile.substring(0, _inputFile.lastIndexOf('.')) + "_processed.dot";
+            System.out.println("Ainur output schedule file name defaulted to: " + _outputFile);
         }
-
-        System.out.println(cmdLine.getArgList());
     }
 
     public void displayUsage() {
@@ -57,7 +69,7 @@ public class Cli {
         System.exit(0);
     }
 
-    public Options establishOptions() {
+    private Options establishOptions() {
         Options options = new Options();
 
         options.addOption("p", true, "use <arg> cores for execution in parallel "
