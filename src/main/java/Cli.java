517d741c55785870e480b529fd1b19d91c4eb5eb
Nathan Cairns <nathancairns777@gmail.com>
2018-08-01 00:50:45 +1200

    Better usage message

--- src/main/java/Cli.java
+++ src/main/java/Cli.java
@@ -6,27 +6,25 @@ public class Cli {
     boolean _visualise;
     int _cores;
     String _outputFile;
+    Options _options;
 
     public Cli(String[] args) {
         _args = args;
         _visualise = false;
         _cores = 1; // TODO default may change when this is implemented
+        _options = establishOptions();
     }
 
     public void parse() throws ParseException {
         // Apache Commons CLI: Definition Stage
-        Options options = establishOptions();
 
         // Apache Commons CLI: Parsing Stage
         DefaultParser clParse = new DefaultParser();
-        CommandLine cmdLine = clParse.parse(options, _args);
+        CommandLine cmdLine = clParse.parse(_options, _args);
 
         // Apache Commons CLI: Interrogation Stage
         if (cmdLine.hasOption("h") || cmdLine.getArgList().size() < 2) {
-            HelpFormatter helpFormatter =   new HelpFormatter();
-
-            helpFormatter.printHelp(Config.APP_NAME, options);
-            System.exit(0);
+            displayUsage();
         }
         if (cmdLine.hasOption("p")) {
             _cores = Integer.parseInt(cmdLine.getOptionValue("p"));
@@ -43,6 +41,20 @@ public class Cli {
         }
 
         System.out.println(cmdLine.getArgList());
+    }
+
+    public void displayUsage() {
+        HelpFormatter helpFormatter = new HelpFormatter();
+
+        String customMessage = new StringBuilder()
+                .append("java -jar " + Config.APP_NAME + ".jar INPUT.dot P [Option]")
+                .append(String.format("\n\nINPUT.dot%9s%s", "", "a task graph with integer weights in dot format"))
+                .append(String.format("\nP%17s%s", "", "number of processors to schedule the INPUT graph on"))
+                .append("\n\nOptional:")
+                .toString();
+
+        helpFormatter.printHelp(customMessage, _options);
+        System.exit(0);
     }
 
     public Options establishOptions() {
