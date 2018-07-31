32b0b04b414472f6a4422c3054346e958b101f66
emipeanz <epea390@aucklanduni.ac.nz>
2018-07-31 17:00:23 +1200

    Add skeleton class for pruner algorithm

---
+++ src/main/java/algorithm/Heuristics/BustersPruner.java
@@ -0,0 +1,15 @@
+package algorithm.Heuristics;
+
+import common.graph.Graph;
+import common.schedule.Schedule;
+
+/**
+ * This is Busters Pruner class
+ */
+public class BustersPruner implements Arborist {
+
+    //TODO - implementation
+    public boolean prune(Graph graph, Schedule schedule) {
+        return false;
+    }
+}
