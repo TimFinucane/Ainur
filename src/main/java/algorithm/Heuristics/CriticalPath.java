e51392114433756ab04417b410820931d5531303
emipeanz <epea390@aucklanduni.ac.nz>
2018-07-31 16:58:12 +1200

    Add CriticalPath class to implement LowerBound

---
+++ src/main/java/algorithm/Heuristics/CriticalPath.java
@@ -0,0 +1,20 @@
+package algorithm.Heuristics;
+
+import common.graph.Graph;
+import common.graph.Node;
+import common.schedule.Schedule;
+
+import java.util.List;
+
+public class CriticalPath implements LowerBound {
+
+    //TODO - implementation
+    public int estimate(Graph graph, Schedule schedule, List<Node> nodesToVisit) {
+        return 0;
+    }
+
+    //TODO - implementation
+    public int estimate(Graph graph, Schedule schedule) {
+        return 0;
+    }
+}
