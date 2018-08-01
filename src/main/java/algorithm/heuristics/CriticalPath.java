9f73b7656f655edfd6d59a5bf16519d03581c682
maddiebeagley <beagleymaddie@gmail.com>
2018-08-01 17:17:53 +1200

    Initial commit

--- src/main/java/algorithm/heuristics/CriticalPath.java
+++ src/main/java/algorithm/heuristics/CriticalPath.java
@@ -2,8 +2,11 @@ package algorithm.heuristics;
 
 import common.graph.Graph;
 import common.graph.Node;
+import common.schedule.Processor;
 import common.schedule.Schedule;
 
+import java.security.Provider;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,11 +19,16 @@ public class CriticalPath implements LowerBound {
      * Method provides an estimate of the lower bound based on parameters provided
      * @param graph : Graph
      * @param schedule : Schedule
-     * @param nodesToVisit : List<Node>
+     * @param nextNodesToVisit : List<Node>
      * @return estimate : int
      */
     //TODO - implementation
-    public int estimate(Graph graph, Schedule schedule, List<Node> nodesToVisit) {
+    public int estimate(Graph graph, Schedule schedule, List<Node> nextNodesToVisit) {
+
+        for (Processor processor : schedule.getProcessors()) {
+
+        }
+
         return 0;
     }
 
