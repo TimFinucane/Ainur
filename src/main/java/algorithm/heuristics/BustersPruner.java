31fa0006d19383065d9ef0a0a1e230271533e643
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-08-01 18:27:57 +1200

    Implement prune method functionality

--- src/main/java/algorithm/heuristics/BustersPruner.java
+++ src/main/java/algorithm/heuristics/BustersPruner.java
@@ -25,13 +25,23 @@ public class BustersPruner implements Arborist {
     public boolean prune(Graph graph, Schedule schedule, Pair<Processor, Task> taskPair) {
 
         // The time that the current latest finishing task in the schedule starts
-        int latestTaskStart = schedule.getTotalTime();
+        int latestTaskStart = 0;
 
-        // TODO: sort out what this is
-        int taskStartTime = 0;
+        // This is a bit messy, add functionality for Processor to do this?
+        for (Processor processor : schedule.getProcessors()) {
+            for (Task task : processor.getTasks()) {
+                if (latestTaskStart < task.getStartTime()) {
+                    latestTaskStart = task.getStartTime();
+                }
+            }
+        }
+
+        // Get starting time of task to add
+        Task taskToAdd = taskPair.getValue();
+        int taskToAddStartTime = taskToAdd.getStartTime();
 
         // If the current added task starts BEFORE the current latest finishing task starts in the schedule
-        if (latestTaskStart > taskStartTime) {
+        if (latestTaskStart > taskToAddStartTime) {
             return true; // Prune
         } else {
             return false; // Don't prune
