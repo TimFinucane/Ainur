a9af22369bc5b3a6871470eaff98b76a62c88a9e
Nathan Cairns <nathancairns777@gmail.com>
2018-08-01 13:57:53 +1200

    Remove unused imports

--- src/main/java/cli/MilestoneOneCli.java
+++ src/main/java/cli/MilestoneOneCli.java
@@ -11,8 +11,6 @@ import io.dot.DotGraphReader;
 import io.dot.DotScheduleWriter;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A class implementing the abstract Cli class.
