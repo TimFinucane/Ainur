ac99b456f553a658ae158caaca222f777d88352a
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-08-01 18:49:06 +1200

    Add unit test for start time pruning method, unsure if succeeds as cannot run

--- src/test/java/algorithm/heuristics/StartTimePrunerTests.java
+++ src/test/java/algorithm/heuristics/StartTimePrunerTests.java
@@ -1,7 +1,27 @@
 package algorithm.heuristics;
 
+import common.graph.Node;
+import common.schedule.Processor;
+import common.schedule.Schedule;
+import common.schedule.Task;
+import javafx.util.Pair;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StartTimePrunerTests {
 
-    
+    @Test
+    public void testEmptyGraph() {
+
+        // Arrange
+        Arborist pruner = new StartTimePruner();
+
+        Schedule schedule = new Schedule(1);
+        Pair<Processor, Task> taskPair = new Pair<>(null, new Task(1, new Node(1, "Stub")));
+
+        //Act / Assert
+        Assert.assertFalse(pruner.prune(null, schedule, taskPair));
+
+    }
 
 }
