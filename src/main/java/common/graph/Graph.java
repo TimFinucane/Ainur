59bf4e99aa563123efaaf1aaa82faaa7713b4fe8
Timothy Finucane <timfinucane@outlook.com>
2018-08-02 23:09:27 +1200

    Setup graph to use id for more efficient accesses

--- src/main/java/common/graph/Graph.java
+++ src/main/java/common/graph/Graph.java
@@ -9,38 +9,35 @@ import java.util.Map;
  * Holds references to all nodes and edges in a Graph.
  */
 public class Graph {
-    private final List<Node>                _nodes;
-
+    private final List<Node>        _nodes = new ArrayList<>();
     // Storage of edges is relative to how nodes access them.
-    // N.B. The key is the node name as of now.
-    // TODO: Look into using the slightly faster ArrayList, where nodes have indices assigned to them.
-    private final Map<String, List<Edge>>   _incomingEdges = new HashMap<>();
-    private final Map<String, List<Edge>>   _outgoingEdges = new HashMap<>();
-
-    private final List<Node>                _entryPoints = new ArrayList<>();
+    private final List<List<Edge>>  _incomingEdges = new ArrayList<>();
+    private final List<List<Edge>>  _outgoingEdges = new ArrayList<>();
+    private final List<Node>        _entryPoints = new ArrayList<>(); // Starting nodes of graph
 
     /**
      * Default constructor for a Graph object
      */
     public Graph(List<Node> nodes, List<Edge> edges) {
-        _nodes = nodes;
-
         // Construct the incoming and outgoing edges, relative to the nodes connected to them
-        for( Node node : nodes )
-        {
-            _outgoingEdges.put(node.getLabel(), new ArrayList<>());
-            _incomingEdges.put(node.getLabel(), new ArrayList<>());
+        int id = 0;
+        for( Node node : nodes ) {
+            _nodes.add(new Node(node.getComputationCost(), node.getLabel(), id));
+            _outgoingEdges.add(new ArrayList<>());
+            _incomingEdges.add(new ArrayList<>());
+
+            id++;
         }
 
-        for( Edge edge : edges )
-        {
-            _incomingEdges.get(edge.getDestinationNode().getLabel()).add(edge);
-            _outgoingEdges.get(edge.getOriginNode().getLabel()).add(edge);
+        // Add every edge to it's incoming node edge list and outgoing node edge list
+        for( Edge edge : edges ) {
+            _incomingEdges.get(edge.getDestinationNode().getId()).add(edge);
+            _outgoingEdges.get(edge.getOriginNode().getId()).add(edge);
         }
 
         // Find the entry points of the graph and cache them
         for( Node node : _nodes )
-            if( _incomingEdges.get(node.getLabel()).isEmpty() )
+            if( _incomingEdges.get(node.getId()).isEmpty() )
                 _entryPoints.add(node);
     }
 
@@ -55,20 +52,20 @@ public class Graph {
      * Get all outgoing edges associated to a particular node
      */
     public List<Edge> getOutgoingEdges(Node node) {
-        return _outgoingEdges.get(node.getLabel());
+        return _outgoingEdges.get(node.getId());
     }
 
     /**
      * Get all incoming edges associated to a particular node
      */
     public List<Edge> getIncomingEdges(Node node) {
-        return _incomingEdges.get(node.getLabel());
+        return _incomingEdges.get(node.getId());
     }
 
     /**
      * Gets the number of nodes in the graph
      */
     public int        size() {
-        return _entryPoints.size();
+        return _nodes.size();
     }
 }
