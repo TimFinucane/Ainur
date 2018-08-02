f3e1979e56e60426e688afddbdb7ff1a7b920df8
emipeanz <epea390@aucklanduni.ac.nz>
2018-08-03 11:26:57 +1200

    Finish off test by comparing written .dot output files schedules max time to that given to us as an answer

--- src/test/java/common/AlgorithmIntegrationTests.java
+++ src/test/java/common/AlgorithmIntegrationTests.java
@@ -17,10 +17,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
@@ -76,7 +74,7 @@ public class AlgorithmIntegrationTests {
         // Check that output file is all good
         File outputFile = new File("data/graphs/Nodes_7_OutTree_processed.dot");
         assertTrue(outputFile.exists());
-        //assertEquals(graphFile.getName() + "_processed", outputFile.getName());
+
         InputStream outputGraphStream = null;
         try {
             outputGraphStream = new FileInputStream(outputFile);
@@ -85,9 +83,50 @@ public class AlgorithmIntegrationTests {
             fail();
         }
 
-        
-
         // Somehow make sure schedule given by dot file matches schedule given by manually making one with methods
+        // Convert schedules to graphs to use in comparison
+        String schedule1 = "digraph \"Processor#1\" {\n" +
+                "\t0\t [Weight=5];\n" +
+                "\t3\t [Weight=6];\n" +
+                "\t0 -> 3\t [Weight=0];\n" +
+                "\t1\t [Weight=6];\n" +
+                "\t3 -> 1\t [Weight=0];\n" +
+                "\t6\t [Weight=7];\n" +
+                "\t1 -> 6\t [Weight=0];\n" +
+                "\t4\t [Weight=4];\n" +
+                "\t6 -> 4\t [Weight=0];\n" +
+                "}\n";
+        InputStream stream1 = new ByteArrayInputStream(schedule1.getBytes(StandardCharsets.UTF_8));
+        GraphReader reader1 = new DotGraphReader(stream1);
+        Graph graph1 = reader1.read();
+
+        String schedule2 = "digraph \"Processor#0\" {\n" +
+                "\t2\t [Weight=5];\n" +
+                "\t5\t [Weight=7];\n" +
+                "\t2 -> 5\t [Weight=0];\n" +
+                "}\n";
+        InputStream stream2 = new ByteArrayInputStream(schedule2.getBytes(StandardCharsets.UTF_8));
+        GraphReader reader2 = new DotGraphReader(stream2);
+        Graph graph2 = reader2.read();
+
+        //Make Schedules
+        Schedule outputSchedule = new Schedule(2);
+        int startTime = 0;
+        Processor p = outputSchedule.getProcessors().get(0);
+            for(Node n : graph1.getNodes()){
+                p.addTask(new Task(startTime, n));
+                startTime = startTime + n.getComputationCost();
+            }
+
+        startTime = 0;
+        Processor p1 = outputSchedule.getProcessors().get(1);
+            for(Node n : graph2.getNodes()){
+                p1.addTask(new Task(startTime, n));
+                startTime = startTime + n.getComputationCost();
+            }
+
+        assertEquals(28, outputSchedule.getTotalTime());
+
     }
 
 
