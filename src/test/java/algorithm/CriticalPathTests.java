04a91e296065f1132f6550a8afde702c16851b97
maddiebeagley <beagleymaddie@gmail.com>
2018-08-02 15:38:00 +1200

    Add test that verifies that critical path works as expected when passed a partial schedule.

--- src/test/java/algorithm/CriticalPathTests.java
+++ src/test/java/algorithm/CriticalPathTests.java
@@ -6,6 +6,7 @@ import common.graph.Edge;
 import common.graph.Graph;
 import common.graph.Node;
 import common.schedule.Schedule;
+import common.schedule.Task;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -16,7 +17,7 @@ import static org.junit.Assert.assertEquals;
 /**
  * Basic class to test the functionality of the critical path finding implementation.
  */
-public class CriticalPathTest {
+public class CriticalPathTests {
 
     /**
      * Test checks that the critical path implementation generates a correct answer and runs without error.
@@ -136,6 +137,51 @@ public class CriticalPathTest {
         assertEquals(criticalPath, 15);
     }
 
+    /**
+     * Verifies that the algorithm runs correctly when passed a partial schedule that has been populated.
+     */
+    @Test
+    public void getCriticalPathWithPopulatedSchedule(){
+        //Generating dummy data for a simple graph and schedule
+        Node nodeA = new Node(3, "A");
+        Node nodeB = new Node(4, "B");
+        Node nodeC = new Node(5, "C");
+        Node nodeD = new Node(1, "D");
+        Node nodeE = new Node(10, "E");
+        Edge edgeAB = new Edge(nodeA, nodeB, 2);
+        Edge edgeAC = new Edge(nodeA, nodeC, 1);
+        Edge edgeCD = new Edge(nodeC, nodeD, 1);
+        Edge edgeCE = new Edge(nodeC, nodeE, 1);
 
+        List<Node> nodes = new ArrayList<>();
+        nodes.add(nodeA);
+        nodes.add(nodeB);
+        nodes.add(nodeC);
+        nodes.add(nodeD);
+        nodes.add(nodeE);
+
+        List<Node> nodesToVisit = new ArrayList<>();
+        nodesToVisit.add(nodeB);
+        nodesToVisit.add(nodeC);
+
+        List<Edge> edges = new ArrayList<>();
+        edges.add(edgeAB);
+        edges.add(edgeAC);
+        edges.add(edgeCD);
+        edges.add(edgeCE);
+
+        Graph graph = new Graph(nodes, edges);
+
+        Task taskA = new Task(0, nodeA);
+
+        Schedule schedule = new Schedule(1);
+        schedule.getProcessors().get(0).addTask(taskA);
+
+        LowerBound lowerBound = new CriticalPath();
+
+        // Calls critical path method with dummy data.
+        int criticalPath = lowerBound.estimate(graph, schedule, nodesToVisit);
+        assertEquals(criticalPath, 15);
+    }
 
 }
