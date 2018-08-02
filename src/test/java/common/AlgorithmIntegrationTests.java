1b7d0af80f7f042d00fa664d5e432a47e9b31c74
emipeanz <epea390@aucklanduni.ac.nz>
2018-08-03 11:33:52 +1200

    Add test for Nodes_8_Random.dot test file

--- src/test/java/common/AlgorithmIntegrationTests.java
+++ src/test/java/common/AlgorithmIntegrationTests.java
@@ -129,6 +129,107 @@ public class AlgorithmIntegrationTests {
 
     }
 
+    @Test
+    public void testNodes_8_RandomGraph() {
 
+        // Set up File
+        File graphFile = new File("data/graphs/Nodes_8_Random.dot");
+        InputStream graphStream = null;
+        try {
+            graphStream = new FileInputStream(graphFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            fail();
+        }
+        GraphReader reader = new DotGraphReader(graphStream);
+        Graph graph = reader.read();
+
+        //Assert that graph is as expected
+        Node entryNode = graph.getEntryPoints().get(0);
+
+        assertEquals(entryNode.getLabel(), "0");
+        assertEquals(35, entryNode.getComputationCost());
+        assertEquals(8, graph.size());
+
+
+        Algorithm algorithm = new DFSAlgorithm(2, new IsNotAPruner(), new NaiveBound());
+
+        algorithm.start(graph);
+        Schedule resultManual = algorithm.getCurrentBest();
+
+        assertEquals(581, resultManual.getTotalTime());
+        // Now run graph through CLI
+        String[] args = {"data/graphs/Nodes_8_Random.dot", "2"};
+        Cli cli = new Cli(args) {
+            @Override
+            protected Schedule startScheduling(Graph graph) {
+                Algorithm algorithm = new DFSAlgorithm(2, new IsNotAPruner(), new NaiveBound());
+
+                algorithm.start(graph);
+                Schedule result = algorithm.getCurrentBest();
+                return result;
+            }
+        };
+        cli.parse();
+
+        // Check that output file is all good
+        File outputFile = new File("data/graphs/Nodes_8_Random.dot");
+        assertTrue(outputFile.exists());
+
+        InputStream outputGraphStream = null;
+        try {
+            outputGraphStream = new FileInputStream(outputFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            fail();
+        }
+
+        // Somehow make sure schedule given by dot file matches schedule given by manually making one with methods
+        // Convert schedules to graphs to use in comparison
+        String schedule1 = "digraph \"Processor#0\" {\n" +
+                "\t0\t [Weight=35];\n" +
+                "\t2\t [Weight=176];\n" +
+                "\t0 -> 2\t [Weight=0];\n" +
+                "\t4\t [Weight=176];\n" +
+                "\t2 -> 4\t [Weight=0];\n" +
+                "\t6\t [Weight=141];\n" +
+                "\t4 -> 6\t [Weight=0];\n" +
+                "\t7\t [Weight=53];\n" +
+                "\t6 -> 7\t [Weight=0];\n" +
+                "}\n";
+        InputStream stream1 = new ByteArrayInputStream(schedule1.getBytes(StandardCharsets.UTF_8));
+        GraphReader reader1 = new DotGraphReader(stream1);
+        Graph graph1 = reader1.read();
+
+        String schedule2 = "digraph \"Processor#1\" {\n" +
+                "\t1\t [Weight=88];\n" +
+                "\t3\t [Weight=159];\n" +
+                "\t1 -> 3\t [Weight=0];\n" +
+                "\t5\t [Weight=141];\n" +
+                "\t3 -> 5\t [Weight=0];\n" +
+                "}\n";
+        InputStream stream2 = new ByteArrayInputStream(schedule2.getBytes(StandardCharsets.UTF_8));
+        GraphReader reader2 = new DotGraphReader(stream2);
+        Graph graph2 = reader2.read();
+
+        //Make Schedules
+        Schedule outputSchedule = new Schedule(2);
+        int startTime = 0;
+        Processor p = outputSchedule.getProcessors().get(0);
+        for(Node n : graph1.getNodes()){
+            p.addTask(new Task(startTime, n));
+            startTime = startTime + n.getComputationCost();
+        }
+
+        startTime = 0;
+        Processor p1 = outputSchedule.getProcessors().get(1);
+        for(Node n : graph2.getNodes()){
+            p1.addTask(new Task(startTime, n));
+            startTime = startTime + n.getComputationCost();
+        }
+
+        assertEquals(581, outputSchedule.getTotalTime());
+
+    }
 
 }
