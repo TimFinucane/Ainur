8e33cbbc5955c0162b5abb38632f5a3c5af711cb
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-08-03 14:41:41 +1200

    Add outline for further test

--- src/test/java/algorithm/Validator.java
+++ src/test/java/algorithm/Validator.java
@@ -1,6 +1,5 @@
 package algorithm;
 
-import com.sun.javafx.scene.control.skin.VirtualFlow;
 import common.graph.Edge;
 import common.graph.Graph;
 import common.graph.Node;
@@ -16,33 +15,43 @@ public class Validator {
 
     public static boolean isValid(Graph graph, Schedule schedule) {
 
+        // Check for dependent nodes of tasks and ensure their tasks finish after their parent task is
+        // finished plus any necessary transmission time.
         for (Processor processor : schedule.getProcessors()) {
             List<Task> tasks = processor.getTasks();
 
             for (Task task : tasks) {
 
-                // Check all dependent nodes are after task finishes and after any necessary transmission time
+                // Loop through all outgoing edges of the graph
                 for (Edge edge : graph.getOutgoingEdges(task.getNode())) {
 
                     Node node = edge.getDestinationNode();
-
                     Pair<Processor, Task> dependentTask = schedule.findTask(node);
 
                     // Check if tasks are on the same processor
                     if (dependentTask.getKey() == processor) {
+                        // Check if dependentTask's start time comes before the task's end time
                         if (dependentTask.getValue().getStartTime() < task.getEndTime()) {
-                            return false;
+                            return false; // Invalid
                         }
                     } else {
+                        /// Check if dependentTask's stert time comes before the task's end time plus transmission cost
                         if (dependentTask.getValue().getStartTime() < task.getEndTime() + edge.getCost()) {
-                            return false;
+                            return false; // Invalid
                         }
                     }
-
                 }
             }
-
         }
+
+        //Check that Tasks do not overlap on a processor
+        for (Processor processor : schedule.getProcessors()) {
+            List<Task> tasks = new ArrayList<>();
+
+            
+        }
+
+        return true;
 
     }
 
