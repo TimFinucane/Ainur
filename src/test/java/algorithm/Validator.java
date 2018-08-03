65f46789a2a3c7e4a25e4b067592c65cb0443697
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-08-03 14:17:22 +1200

    Skeleton algorithm lay out

--- src/test/java/algorithm/Validator.java
+++ src/test/java/algorithm/Validator.java
@@ -16,25 +16,32 @@ public class Validator {
 
     public static boolean isValid(Graph graph, Schedule schedule) {
 
-        List<Task> tasks = new ArrayList<>();
-
         for (Processor processor : schedule.getProcessors()) {
-            for (Task task : processor.getTasks()) {
-                tasks.add(task);
+            List<Task> tasks = processor.getTasks();
+
+            for (Task task : tasks) {
+
+                // Check all dependent nodes are after task finishes and after any necessary transmission time
+                for (Edge edge : graph.getOutgoingEdges(task.getNode())) {
+
+                    Node node = edge.getDestinationNode();
+
+                    Pair<Processor, Task> dependentTask = schedule.findTask(node);
+
+                    // Check if tasks are on the same processor
+                    if (dependentTask.getKey() == processor) {
+                        if (dependentTask.getValue().getStartTime() < task.getEndTime()) {
+                            return false;
+                        }
+                    } else {
+                        if (dependentTask.getValue().getStartTime() < task.getEndTime() + edge.getCost()) {
+                            return false;
+                        }
+                    }
+
+                }
             }
-        }
 
-        for (Task task : tasks) {
-            // The outgoing edges of the node in question
-            List<Edge> edges = graph.getOutgoingEdges(task.getNode());
-
-            // Check all dependent nodes are after task finishes and after any necessary transmission time
-            for (Edge edge : edges) {
-                Node node = edge.getDestinationNode();
-
-                Pair<Processor, Task> dependentTask = schedule.findTask(node);
-                
-            }
         }
 
     }
