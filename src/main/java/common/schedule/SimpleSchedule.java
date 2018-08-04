95a3e79167fbf4e164ab88f01d998effb9a446bf
Timothy Finucane <timfinucane@outlook.com>
2018-08-04 19:08:39 +1200

    Add comments to SimpleSchedule

--- src/main/java/common/schedule/SimpleSchedule.java
+++ src/main/java/common/schedule/SimpleSchedule.java
@@ -11,11 +11,11 @@ public class SimpleSchedule extends Schedule {
     // TODO: This is probably somewhat inefficient, especially with memory. Can we find a way?
     private List<Task>[] _processors;
 
-    public SimpleSchedule(int numProcessors, int nodes) {
-        this(numProcessors);
-
-        _tasks = new ArrayList<>(Collections.nCopies(nodes, null));
-    }
+    /**
+     * Creates the schedule with the given number of processors and a
+     * dynamic number of nodes
+     * @param numProcessors fixed number of processors in the schedule
+     */
     public SimpleSchedule(int numProcessors) {
         super(numProcessors);
         _processors = new ArrayList[numProcessors]; // Java generics means i have to do it like this
@@ -24,6 +24,21 @@ public class SimpleSchedule extends Schedule {
             _processors[i] = new ArrayList<>();
     }
 
+    /**
+     * Creates the schedule with a fixed number of processors and
+     * a fixed (kinda) number of nodes.
+     * @param numProcessors fixed number of processors in the schedule
+     * @param nodes Pre-creates this many nodes for (very slight) optimization
+     */
+    public SimpleSchedule(int numProcessors, int nodes) {
+        this(numProcessors);
+
+        _tasks = new ArrayList<>(Collections.nCopies(nodes, null));
+    }
+
+    /**
+     * Copies the schedule into this new schedule
+     */
     @SuppressWarnings("unchecked")
     public SimpleSchedule(SimpleSchedule other) {
         super(other._numProcessors);
@@ -37,6 +52,9 @@ public class SimpleSchedule extends Schedule {
             _processors[i] = new ArrayList<>(other._processors[i]);
     }
 
+    /**
+     * @see Schedule#addTask(Task)
+     */
     @Override
     public void addTask(Task task) {
         if(size(task.getProcessor()) > 0 && (getLatest(task.getProcessor()).getStartTime() > task.getStartTime()))
@@ -48,6 +66,9 @@ public class SimpleSchedule extends Schedule {
         _tasks.set(task.getNode().getId(), task);
         _processors[task.getProcessor()].add(task);
     }
+    /**
+     * @see Schedule#removeTask(Task)
+     */
     @Override
     public void removeTask(Task task) {
         if(getLatest(task.getProcessor()).getNode() != task.getNode())
@@ -57,21 +78,33 @@ public class SimpleSchedule extends Schedule {
         _processors[task.getProcessor()].remove(size(task.getProcessor()) - 1);
     }
 
+    /**
+     * @see Schedule#findTask(Node)
+     */
     @Override
     public Task findTask(Node node) {
         return _tasks.size() > node.getId() ? _tasks.get(node.getId()) : null;
     }
 
+    /**
+     * @see Schedule#getLatest()
+     */
     @Override
     public Task getLatest(int processor) {
         return _processors[processor].size() > 0 ? _processors[processor].get(_processors[processor].size() - 1) : null;
     }
 
+    /**
+     * @see Schedule#getTasks(int)
+     */
     @Override
     public List<Task> getTasks(int processor) {
         return Collections.unmodifiableList(_processors[processor]);
     }
 
+    /**
+     * @see Schedule#size()
+     */
     @Override
     public int size(int processor) {
         return _processors[processor].size();
