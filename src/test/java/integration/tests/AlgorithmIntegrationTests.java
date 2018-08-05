c2c986a75ac6d94089c02437d01b60eed33a0b9b
Timothy Finucane <timfinucane@outlook.com>
2018-08-05 20:45:32 +1200

    Test all pruners and lower bounds in tests

--- src/test/java/integration/tests/AlgorithmIntegrationTests.java
+++ src/test/java/integration/tests/AlgorithmIntegrationTests.java
@@ -2,10 +2,7 @@ package integration.tests;
 
 import algorithm.Algorithm;
 import algorithm.DFSAlgorithm;
-import algorithm.heuristics.CriticalPath;
-import algorithm.heuristics.IsNotAPruner;
-import algorithm.heuristics.NaiveBound;
-import algorithm.heuristics.StartTimePruner;
+import algorithm.heuristics.*;
 import cli.Cli;
 import common.categories.GandalfIntegrationTestsCategory;
 import common.graph.Graph;
@@ -345,8 +342,13 @@ public class AlgorithmIntegrationTests {
 
         assertEquals(10, graph.size());
 
-
-        Algorithm algorithm = new DFSAlgorithm(4, new StartTimePruner(), new NaiveBound());
+        Algorithm algorithm = new DFSAlgorithm(
+            4,
+            (pruningGraph, pruningSchedule, pruningTask) ->
+                new StartTimePruner().prune(pruningGraph, pruningSchedule, pruningTask)
+                    || new ProcessorOrderPruner().prune(pruningGraph, pruningSchedule, pruningTask),
+            new CriticalPath()
+        );
 
         algorithm.start(graph);
         //Manually start algorithm on graph and check that final answer is correct
@@ -359,7 +361,13 @@ public class AlgorithmIntegrationTests {
         Cli cli = new Cli(args) {
             @Override
             protected Schedule startScheduling(Graph graph) {
-                Algorithm algorithm = new DFSAlgorithm(4, new StartTimePruner(), new NaiveBound());
+                Algorithm algorithm = new DFSAlgorithm(
+                    4,
+                    (pruningGraph, pruningSchedule, pruningTask) ->
+                        new StartTimePruner().prune(pruningGraph, pruningSchedule, pruningTask)
+                            || new ProcessorOrderPruner().prune(pruningGraph, pruningSchedule, pruningTask),
+                    new CriticalPath()
+                );
 
                 algorithm.start(graph);
                 Schedule result = algorithm.getCurrentBest();
@@ -405,12 +413,22 @@ public class AlgorithmIntegrationTests {
 
         assertEquals(11, graph.size());
 
-
-        Algorithm algorithm = new DFSAlgorithm(4, new StartTimePruner(), new NaiveBound());
-
+        Algorithm algorithm = new DFSAlgorithm(
+            4,
+            (pruningGraph, pruningSchedule, pruningTask) ->
+                new StartTimePruner().prune(pruningGraph, pruningSchedule, pruningTask)
+                    || new ProcessorOrderPruner().prune(pruningGraph, pruningSchedule, pruningTask),
+            new CriticalPath()
+        );
         algorithm.start(graph);
         //Manually start algorithm on graph and check that final answer is correct
         Schedule resultManual = algorithm.getCurrentBest();
+
+        for(int i = 0; i < resultManual.getNumProcessors(); ++i) {
+            System.out.println("Processor " + String.valueOf(i));
+            for(Task task : resultManual.getTasks(i))
+                System.out.println( String.valueOf(task.getStartTime()) + "(" + String.valueOf(task.getNode().getComputationCost()) + "), " + task.getNode().getLabel());
+        }
 
         assertEquals(227, resultManual.getEndTime());
 
@@ -419,7 +437,13 @@ public class AlgorithmIntegrationTests {
         Cli cli = new Cli(args) {
             @Override
             protected Schedule startScheduling(Graph graph) {
-                Algorithm algorithm = new DFSAlgorithm(4, new StartTimePruner(), new NaiveBound());
+                Algorithm algorithm = new DFSAlgorithm(
+                    4,
+                    (pruningGraph, pruningSchedule, pruningTask) ->
+                        new StartTimePruner().prune(pruningGraph, pruningSchedule, pruningTask)
+                            || new ProcessorOrderPruner().prune(pruningGraph, pruningSchedule, pruningTask),
+                    new CriticalPath()
+                );
 
                 algorithm.start(graph);
                 Schedule result = algorithm.getCurrentBest();
