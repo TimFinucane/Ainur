c9caeb85d17af7c9559bae39a9f2acf2f0a2cc2d
Timothy Finucane <timfinucane@outlook.com>
2018-08-05 20:35:20 +1200

    Correct arborist to be harmonous with StartTimePruner

--- src/main/java/algorithm/heuristics/ProcessorOrderPruner.java
+++ src/main/java/algorithm/heuristics/ProcessorOrderPruner.java
@@ -10,11 +10,19 @@ import common.schedule.Task;
 public class ProcessorOrderPruner implements Arborist {
     @Override
     public boolean prune(Graph graph, Schedule schedule, Task toBeAdded) {
+        if(schedule.size(toBeAdded.getProcessor()) > 0)
+            return false;
         // Cull branch if there is any processor before the one on which the task being placed which has no tasks or
         // its first task is ordered after this task (by node id).
         for(int i = 0; i < toBeAdded.getProcessor(); ++i) {
-            if(schedule.size(i) == 0
-                || schedule.getTasks(i).get(0).getNode().getId() > toBeAdded.getNode().getId())
+            if(schedule.size(i) == 0) // Task which starts first should come next
+                return true;
+            // Order by startTime then (if there is no chance of one depending on the other) node id
+            Task firstTask = schedule.getTasks(i).get(0);
+            if(toBeAdded.getStartTime() < firstTask.getStartTime()
+                || (toBeAdded.getStartTime() == firstTask.getStartTime()
+                    && toBeAdded.getNode().getId() < firstTask.getNode().getId()
+                    && firstTask.getNode().getComputationCost() > 0))
                 return true;
         }
         return false;
