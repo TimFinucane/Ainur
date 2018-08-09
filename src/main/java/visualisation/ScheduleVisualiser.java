9da0ce73c4956e5e5bd54f31a4653c9cebab11f2
maddiebeagley <beagleymaddie@gmail.com>
2018-08-09 16:36:24 +1200

    Schedule visualiser generates appropriately sized grid to store schedules.

--- src/main/java/visualisation/ScheduleVisualiser.java
+++ src/main/java/visualisation/ScheduleVisualiser.java
@@ -1,14 +1,66 @@
 package visualisation;
 
 import common.schedule.Schedule;
+import javafx.geometry.Bounds;
 import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 
 public class ScheduleVisualiser extends Group {
 
-    public ScheduleVisualiser(){
+    public ScheduleVisualiser(Schedule schedule){
+        int endTime = schedule.getEndTime();
+        int numProc = schedule.getNumProcessors();
     }
 
-    public void update(Schedule schedule) {
+    public ScheduleVisualiser(){
+        update();
+    }
+
+    public void update() {
+
+        Node node = (Node)this.getParent();
+        int numProc = 4;
+        int endTime = 14;
+
+        int colWidth = 750/endTime;
+        int rowHeight = 200/numProc;
+//
+//        Button button = new Button();
+//        button.setMinWidth(100);
+//        button.setMinHeight(50);
+//        button.setText("hello");
+
+
+        GridPane grid = new GridPane();
+
+        for (int i = 0; i < endTime; i++) {
+            ColumnConstraints cc = new ColumnConstraints(colWidth);
+            grid.getColumnConstraints().add(cc);
+        }
+
+        for (int i = 0; i < numProc; i++) {
+            RowConstraints rc = new RowConstraints(rowHeight);
+            grid.getRowConstraints().add(rc);
+        }
+
+
+        System.out.println(grid.toString());
+
+        System.out.println(grid.getColumnConstraints());
+        System.out.println(grid.getRowConstraints());
+
+        grid.add(new Button(), 3, 3);
+
+
+        this.getChildren().add(grid);
+
     }
 
 }
--- src/test/java/visualisation/ScheduleVisualiserTest.java
+++ src/test/java/visualisation/ScheduleVisualiserTest.java
@@ -1,9 +1,26 @@
 package visualisation;
 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import org.junit.Ignore;
+import org.junit.Test;
+
 /**
  * Class used to check rendering of ScheduleVisualiser components.
  */
-public class ScheduleVisualiserTest {
+@Ignore
+public class ScheduleVisualiserTest extends Application{
 
+    @Override
+    public void start(Stage stage) {
+        Group schedule = new ScheduleVisualiser();
+//        schedule.getChildren().add(new Button());
 
+        Scene scene = new Scene(schedule);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
