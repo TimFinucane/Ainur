a809c291345059efb865b48d41e31279deb08c3a
maddiebeagley <beagleymaddie@gmail.com>
2018-08-09 15:29:03 +1200

    Added skeleton class structure for ScheduleVisualiser and associated test class.

---
+++ src/main/java/visualisation/ScheduleVisualiser.java
@@ -0,0 +1,14 @@
+package visualisation;
+
+import common.schedule.Schedule;
+import javafx.scene.Group;
+
+public class ScheduleVisualiser extends Group {
+
+    public ScheduleVisualiser(){
+    }
+
+    public void update(Schedule schedule) {
+    }
+
+}
---
+++ src/test/java/visualisation/ScheduleVisualiserTest.java
@@ -0,0 +1,9 @@
+package visualisation;
+
+/**
+ * Class used to check rendering of ScheduleVisualiser components.
+ */
+public class ScheduleVisualiserTest {
+
+
+}
