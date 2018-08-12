093c09278524abb6651df2907fa2e3ea391b9388
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-08-12 22:20:24 +1200

    Rename Statistics fields

--- src/main/java/visualisation/Statistics.java
+++ src/main/java/visualisation/Statistics.java
@@ -2,22 +2,22 @@ package visualisation;
 
 public class Statistics {
 
-    private int _maxScheduleLength;
-    private int _minScheduleLength;
+    private int _maxScheduleBound;
+    private int _minScheduleBound;
 
     public int getMaxScheduleBound() {
-        return _maxScheduleLength;
+        return _maxScheduleBound;
     }
 
     public void setMaxScheduleBound(int _maxScheduleLength) {
-        this._maxScheduleLength = _maxScheduleLength;
+        this._maxScheduleBound = _maxScheduleLength;
     }
 
     public int getMinScheduleBound() {
-        return _minScheduleLength;
+        return _minScheduleBound;
     }
 
     public void setMinScheduleBound(int _minScheduleLength) {
-        this._minScheduleLength = _minScheduleLength;
+        this._minScheduleBound = _minScheduleLength;
     }
 }
