8104bff06cd444f61cfcfa0dae71c036faf38850
Nathan Cairns <nathancairns777@gmail.com>
2018-08-13 17:12:05 +1200

    Document tests

--- src/test/java/visualisation/GraphVisualiserTest.java
+++ src/test/java/visualisation/GraphVisualiserTest.java
@@ -16,29 +16,51 @@ import java.io.InputStream;
 
 import static junit.framework.TestCase.fail;
 
+/**
+ * Class used to check the rendering of the GraphVisualiser class.
+ */
 @Ignore
 public class GraphVisualiserTest extends Application {
-    /* MACROS */
+
+    // Graph file to load in
     public static final String GRAPH_FILE = "data/graphs/Nodes_11_OutTree.dot";
 
+    /**
+     * Displays a graph visualisation from a static file input.
+     */
     @Override
     public void start(Stage primaryStage) {
+        // Create a graph visualizer from file.
         Graph graph = this.loadGraph(GRAPH_FILE);
         GraphVisualiser gv = new GraphVisualiser(graph);
+
+        // Set up javafx scene
         Scene scene = new Scene(gv);
         primaryStage.setScene(scene);
         primaryStage.show();
 
+        // Test updating.
+        // End result should have 1 highlighted and all other nodes should not be highlighted.
         this.testUpdate(graph.findByLabel("0"), gv);
-
         this.testUpdate(graph.findByLabel("1"), gv);
     }
 
-    @Test
+    /**
+     * Tests the update method of GraphVisualiser
+     *
+     * @param nodeToUpdate The node to highlight
+     * @param gv The graphvisualiser to use
+     */
     public void testUpdate(Node nodeToUpdate, GraphVisualiser gv) {
         gv.update(nodeToUpdate);
     }
 
+    /**
+     * Private helper method for loading in a Ainur graph from a file.
+     *
+     * @param pathname The filepath of the .dot file to read from.
+     * @return The Ainur graph read from the .dot file
+     */
     private Graph loadGraph(String pathname) {
         File graphFile = new File(pathname);
         InputStream is = null;
