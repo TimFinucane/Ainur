c500db1736c0b72e327c113d130537ddd196d5c3
Timothy Finucane <timfinucane@outlook.com>
2018-08-13 23:30:03 +1200

    Move generic methods to helper static class

--- src/main/java/algorithm/DFSAlgorithm.java
+++ src/main/java/algorithm/DFSAlgorithm.java
@@ -2,15 +2,10 @@ package algorithm;
 
 import algorithm.heuristics.pruner.Arborist;
 import algorithm.heuristics.lowerbound.LowerBound;
-import common.graph.Edge;
-import common.graph.Graph;
-import common.graph.Node;
-import common.schedule.Schedule;
-import common.schedule.SimpleSchedule;
-import common.schedule.Task;
-
+import common.graph.*;
+import common.schedule.*;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
+import static algorithm.Helpers.*;
 
 /**
  * A DFS implementation of the Algorithm class.
@@ -133,63 +128,5 @@ public class DFSAlgorithm extends BoundableAlgorithm {
             newSchedule.addTask(new Task(minIndex, earliestStarts[minIndex], last));
             _communicator.update(newSchedule);
         }
-    }
-
-    /**
-     * Calculates the set of available nodes to add to the schedule, derived from adding the current node
-     * to the schedule and using the old set of available nodes.
-     * TODO: Optimize?
-     */
-    private HashSet<Node> calculateNextNodes(Graph graph, SimpleSchedule schedule, HashSet<Node> oldNodes, Node added) {
-        // Construct our new available nodes to pass on by copying available nodes and removing the one we're about
-        // to add
-        HashSet<Node> nextAvailableNodes = new HashSet<>(oldNodes);
-        nextAvailableNodes.remove(added);
-
-        // Now add all the children of the node we are visiting
-        // Check that everything we add has all it's parents in the schedule.
-        // There might be better code to do this or via method?
-        for(Edge edge : graph.getOutgoingEdges(added)) {
-            Node nodeToAdd = edge.getDestinationNode();
-
-            boolean parentsInSchedule = true;
-            for(Edge parentEdge : graph.getIncomingEdges(nodeToAdd)) {
-                if (parentEdge.getOriginNode() != added && !schedule.contains(parentEdge.getOriginNode())) {
-                    parentsInSchedule = false;
-                    break;
-                }
-            }
-            if(parentsInSchedule)
-                nextAvailableNodes.add(nodeToAdd);
-        }
-
-        return nextAvailableNodes;
-    }
-
-    /**
-     * Calculates the earliest times it can add the given node to each processor in the set
-     * TODO: Optimize?
-     */
-    private int[] calculateEarliestTimes(Graph graph, SimpleSchedule schedule, Node node) {
-        // Calculate earliest it can be placed
-        int[] earliests = new int[schedule.getNumProcessors()];
-        for(Edge edge : graph.getIncomingEdges(node)) {
-            Node dependencyNode = edge.getOriginNode();
-            Task item = schedule.findTask(dependencyNode);
-
-            if(item == null)
-                throw new RuntimeException("Chide Tim for not checking a node's parents are in the schedule");
-
-            // If it's on the same processor, just has to be after task end. If not, then it also needs
-            // to be past the communication cost
-            for(int processor = 0; processor < schedule.getNumProcessors(); ++processor)
-                earliests[processor] = Math.max(earliests[processor],
-                    (item.getProcessor() == processor) ? item.getEndTime() :  item.getEndTime() + edge.getCost());
-        }
-        for(int processor = 0; processor < schedule.getNumProcessors(); ++processor)
-            if( schedule.size(processor) > 0 )
-                earliests[processor] = Math.max(earliests[processor], schedule.getLatest(processor).getEndTime());
-
-        return earliests;
     }
 }
---
+++ src/main/java/algorithm/Helpers.java
@@ -0,0 +1,66 @@
+package algorithm;
+
+import common.graph.*;
+import common.schedule.*;
+
+import java.util.HashSet;
+
+public class Helpers {
+    /**
+     * Calculates the set of available nodes to add to the schedule, derived from adding the current node
+     * to the schedule and using the old set of available nodes.
+     * TODO: Optimize?
+     */
+    public static HashSet<Node> calculateNextNodes(Graph graph, Schedule schedule, HashSet<Node> oldNodes, Node added) {
+        // Construct our new available nodes to pass on by copying available nodes and removing the one we're about
+        // to add
+        HashSet<Node> nextAvailableNodes = new HashSet<>(oldNodes);
+        nextAvailableNodes.remove(added);
+
+        // Now add all the children of the node we are visiting
+        // Check that everything we add has all it's parents in the schedule.
+        // There might be better code to do this or via method?
+        for(Edge edge : graph.getOutgoingEdges(added)) {
+            Node nodeToAdd = edge.getDestinationNode();
+
+            boolean parentsInSchedule = true;
+            for(Edge parentEdge : graph.getIncomingEdges(nodeToAdd)) {
+                if (parentEdge.getOriginNode() != added && !schedule.contains(parentEdge.getOriginNode())) {
+                    parentsInSchedule = false;
+                    break;
+                }
+            }
+            if(parentsInSchedule)
+                nextAvailableNodes.add(nodeToAdd);
+        }
+
+        return nextAvailableNodes;
+    }
+
+    /**
+     * Calculates the earliest times it can add the given node to each processor in the set
+     * TODO: Optimize?
+     */
+    public static int[] calculateEarliestTimes(Graph graph, Schedule schedule, Node node) {
+        // Calculate earliest it can be placed
+        int[] earliests = new int[schedule.getNumProcessors()];
+        for(Edge edge : graph.getIncomingEdges(node)) {
+            Node dependencyNode = edge.getOriginNode();
+            Task item = schedule.findTask(dependencyNode);
+
+            if(item == null)
+                throw new RuntimeException("Chide Tim for not checking a node's parents are in the schedule");
+
+            // If it's on the same processor, just has to be after task end. If not, then it also needs
+            // to be past the communication cost
+            for(int processor = 0; processor < schedule.getNumProcessors(); ++processor)
+                earliests[processor] = Math.max(earliests[processor],
+                    (item.getProcessor() == processor) ? item.getEndTime() :  item.getEndTime() + edge.getCost());
+        }
+        for(int processor = 0; processor < schedule.getNumProcessors(); ++processor)
+            if( schedule.size(processor) > 0 )
+                earliests[processor] = Math.max(earliests[processor], schedule.getLatest(processor).getEndTime());
+
+        return earliests;
+    }
+}
