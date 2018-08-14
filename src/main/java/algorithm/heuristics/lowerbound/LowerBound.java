57ebc36493a80f45b24e2a090ff62ebe26229233
emipeanz <epea390@aucklanduni.ac.nz>
2018-08-14 16:48:33 +1200

    Remove unused default estimate method

--- src/main/java/algorithm/heuristics/lowerbound/LowerBound.java
+++ src/main/java/algorithm/heuristics/lowerbound/LowerBound.java
@@ -22,17 +22,5 @@ public interface LowerBound {
      *                     (i.e. all dependencies accounted for in the partial schedule).
      * @return A lower bound of the optimal solution length
      */
-    int estimate(Graph graph, Schedule schedule, List<Node> nodesToVisit);
-
-    /**
-     * Efficiently and heuristically estimates a lower bound for the best possible time of a solution
-     * created using the partial schedule provided.
-     * @param graph The entire graph of the problem
-     * @param schedule A partial/incomplete schedule from which the range of solutions is based
-     * @return A lower bound of the optimal solution length
-     */
-    default int estimate(Graph graph, Schedule schedule){
-        return this.estimate(graph, schedule, new ArrayList<>());
-    }
 
 }
