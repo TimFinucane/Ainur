fb5ed107e48d2a65a94403f42e2b22b6505cac31
emipeanz <epea390@aucklanduni.ac.nz>
2018-08-15 10:45:29 +1200

    Start greedy implementation

--- src/main/java/algorithm/GreedyAlgorithm.java
+++ src/main/java/algorithm/GreedyAlgorithm.java
@@ -3,12 +3,31 @@ package algorithm;
 import common.graph.Graph;
 import common.graph.Node;
 import common.schedule.Schedule;
+import common.schedule.SimpleSchedule;
+import common.schedule.Task;
+
+import java.util.List;
 
 public class GreedyAlgorithm implements Algorithm {
 
     @Override
     public void run(Graph graph, int processors) {
+        SimpleSchedule schedule = new SimpleSchedule(processors);
+        List<Node> nodesToVisit = graph.getEntryPoints();
 
+        while (!(nodesToVisit.isEmpty())){
+            Node currentNode = nodesToVisit.get(0);
+            nodesToVisit.remove(currentNode);
+
+
+            // Check that all parents are there
+
+
+            // For each node, try it on each processor
+            for (int i = 0; i<processors ; i++){
+
+            }
+        }
     }
 
     @Override
