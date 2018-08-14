18a5498dded806b797b4e9be9f606e1a589e10cd
maddiebeagley <beagleymaddie@gmail.com>
2018-08-15 10:19:57 +1200

    Skeleton of class declared.

--- src/main/java/algorithm/GreedyAlgorithm.java
+++ src/main/java/algorithm/GreedyAlgorithm.java
@@ -1,4 +1,33 @@
 package algorithm;
 
-public class GreedyAlgorithm {
+import common.graph.Graph;
+import common.graph.Node;
+import common.schedule.Schedule;
+
+public class GreedyAlgorithm implements Algorithm {
+
+    @Override
+    public void run(Graph graph, int processors) {
+
+    }
+
+    @Override
+    public Schedule getCurrentBest() {
+        return null;
+    }
+
+    @Override
+    public int branchesCulled() {
+        return 0;
+    }
+
+    @Override
+    public int branchesExplored() {
+        return 0;
+    }
+
+    @Override
+    public Node currentNode() {
+        return null;
+    }
 }
