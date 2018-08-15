859e6c3b6c393604ae9c7186c6ccffad65cbe4cd
emipeanz <epea390@aucklanduni.ac.nz>
2018-08-15 16:36:11 +1200

    Re-work greedy algorithm to use while loop only to go through nodes.

--- src/main/java/algorithm/GreedyAlgorithm.java
+++ src/main/java/algorithm/GreedyAlgorithm.java
@@ -22,53 +22,24 @@ public class GreedyAlgorithm implements Algorithm {
         SimpleSchedule schedule = new SimpleSchedule(processors);
         List<Node> nodesToVisit = graph.getEntryPoints();
 
-        Node rootNode = nodesToVisit.get(0);
-        int initCost = nodesToVisit.get(0).getComputationCost();
-
-        // place the node with the smallest computation cost first in the schedule
-        for (Node node : nodesToVisit) {
-            if (node.getComputationCost() < initCost){
-                initCost = node.getComputationCost();
-                rootNode = node;
-            }
-        }
-        // add the node with smallest cost into start of schedule.
-        schedule.addTask(new Task(0, 0, rootNode));
-
-        // next nodes to visit are all those accessible once root node has been added
-        nodesToVisit = new ArrayList<>(Helpers.calculateNextNodes(graph, schedule, new HashSet<>(nodesToVisit), rootNode));
-
         // while there are still nodes that have not been added to the schedule
         while (!(nodesToVisit.isEmpty())) {
-
-            int bestEndTime = Integer.MAX_VALUE;
-            SimpleSchedule curBestSchedule = new SimpleSchedule(processors);
-            Node nodeAdded = null;
-
-            //try adding all nodes to all possible schedules and pick the shortest one.
-            for (Node node : nodesToVisit) {
-                int[] earliestTimes = Helpers.calculateEarliestTimes(graph, schedule, node);
-
-                //add all nodes to all different processors
-                for (int proc = 0; proc < processors; proc++) {
-
-                    //generate a schedule with current node placed at best time on current processor.
-                    SimpleSchedule newSchedule = schedule;
-                    newSchedule.addTask(new Task(proc, earliestTimes[proc], node));
-
-                    // if this schedule has the current best end time, store it.
-                    if (newSchedule.getEndTime() < bestEndTime) {
-                        bestEndTime = newSchedule.getEndTime();
-                        curBestSchedule = newSchedule;
-                        nodeAdded = node;
-                    }
+            // Get current node we're gonna add to the schedule
+            Node curNode = nodesToVisit.get(0);
+            int[] earliestTimes = Helpers.calculateEarliestTimes(graph, schedule, curNode);
+            // Looks at all the possible time that task can be added and find the earliest position
+            Task bestTask = new Task(0, 0, new Node(Integer.MAX_VALUE, "a", 1));
+            for(int i = 0; i < processors; i++){
+                Task t = new Task(i, earliestTimes[i], curNode);
+                if(t.getEndTime() < bestTask.getEndTime()){
+                    bestTask = t;
                 }
             }
 
-            schedule = curBestSchedule;
+            schedule.addTask(bestTask);
 
             // the next nodes to visit are now those accessible from the new best schedule.
-            nodesToVisit = new ArrayList<>(Helpers.calculateNextNodes(graph, schedule, new HashSet<>(nodesToVisit), nodeAdded));
+            nodesToVisit = new ArrayList<>(Helpers.calculateNextNodes(graph, schedule, new HashSet<>(nodesToVisit), bestTask.getNode()));
         }
 
         _schedule = schedule;
