a933f3f85aae27d580f95a6ab91caae80f948a67
Nathan Cairns <nathancairns777@gmail.com>
2018-08-15 14:36:31 +1200

    Create ainurVisualiser files

---
+++ src/main/java/visualisation/AinurVisualiser.java
@@ -0,0 +1,12 @@
+package visualisation;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AinurVisualiser extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ src/test/java/visualisation/AinurVisualiserTest.java
@@ -0,0 +1,4 @@
+package visualisation;
+
+public class AinurVisualiserTest {
+}
