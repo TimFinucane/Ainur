b8d4e8d96de3761978ab3964d7c97ae04297c832
Timothy Finucane <timfinucane@outlook.com>
2018-08-16 20:00:49 +1200

    Add text and marker lines

--- src/main/java/visualisation/ScheduleVisualiser.java
+++ src/main/java/visualisation/ScheduleVisualiser.java
@@ -3,23 +3,29 @@ package visualisation;
 import common.schedule.Schedule;
 import common.schedule.Task;
 import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.chart.Axis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.layout.*;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 /**
  * Class to deal with the visual rendering of a schedule.
  */
 public class ScheduleVisualiser extends VBox {
-    private static final Color FILL_COLOR = Color.LAVENDER;
-    private static final Color BORDER_COLOR = Color.BLACK;
+    private static final Color FILL_COLOUR = Color.LAVENDER;
+    private static final Color BORDER_COLOUR = Color.BLACK;
+    private static final Color TEXT_COLOUR = Color.BLACK;
 
-    private Schedule _schedule;
-    private Canvas _scheduleView;
+    private Schedule    _schedule;
+    private Canvas      _scheduleView;
+    private NumberAxis  _axis;
 
     public ScheduleVisualiser() {
         _scheduleView = new Canvas();
@@ -28,17 +34,26 @@ public class ScheduleVisualiser extends VBox {
         _scheduleView.widthProperty().bind(canvasHolder.widthProperty());
         _scheduleView.heightProperty().bind(canvasHolder.heightProperty());
 
-        // Ensure that when the canvas holder resizes, it redraws the schedule
-        canvasHolder.widthProperty().addListener(e -> draw());
-        canvasHolder.heightProperty().addListener(e -> draw());
+        // Ensure that when we resize, it redraws the schedule
+        widthProperty().addListener(e -> draw());
+        heightProperty().addListener(e -> draw());
 
         // Add the canvas holder, and make sure it takes available height
         getChildren().add(canvasHolder);
         setVgrow(canvasHolder, Priority.ALWAYS);
 
+        // And the number axis
+        _axis = new NumberAxis(0, 100, 10);
+        _axis.setTickLength(10.0);
+        getChildren().add(_axis);
+
+        // And have a nice bit of pad
         setPadding(new Insets(10));
     }
 
+    /**
+     * Called to update the schedule being rendered to screen
+     */
     public void update(Schedule schedule) {
         _schedule = schedule;
         draw();
@@ -57,28 +72,42 @@ public class ScheduleVisualiser extends VBox {
         gc.clearRect(0, 0, getWidth(), getHeight());
 
         // Colours of the tasks
-        gc.setFill(FILL_COLOR);
-        gc.setStroke(BORDER_COLOR);
+        gc.setFill(FILL_COLOUR);
+        gc.setStroke(BORDER_COLOUR);
+        gc.setTextAlign(TextAlignment.CENTER);
+        gc.setTextBaseline(VPos.CENTER);
 
-        double width = gc.getCanvas().getWidth();
-        double height = gc.getCanvas().getHeight();
+        double canvasWidth = gc.getCanvas().getWidth();
+        double canvasHeight = gc.getCanvas().getHeight();
 
-        // The height of each task in the processor
-        double taskHeight = height / _schedule.getNumProcessors();
         // The width of a single unit time in the schedule. End time is padded by 10% so schedule doesn't go to end
-        double unitWidth = width / (_schedule.getEndTime());
+        double unitWidth = canvasWidth / (_schedule.getEndTime());
+        // The height of each task in the processor
+        double taskHeight = canvasHeight / _schedule.getNumProcessors();
 
         // Add each task to position
         for(int processor = 0; processor < _schedule.getNumProcessors(); ++processor) {
             for(Task task : _schedule.getTasks(processor)) {
-                gc.fillRect(
-                    unitWidth * task.getStartTime(),
-                    taskHeight * processor,
-                    unitWidth * task.getNode().getComputationCost(),
-                    taskHeight
-                );
+                double left = unitWidth * task.getStartTime();
+                double top = taskHeight * processor;
+                double width = unitWidth * task.getNode().getComputationCost();
+
+                gc.setFill(FILL_COLOUR);
+                gc.fillRect(left, top,width, taskHeight);
+
+                double centre = left + width / 2;
+                gc.setFill(TEXT_COLOUR);
+                gc.setFont( new Font(gc.getFont().getName(), Math.min(taskHeight / 2, width / 2)) );
+                gc.fillText(task.getNode().getLabel(), centre, top + taskHeight / 2);
             }
         }
-    }
+        // Modify number axis
+        _axis.setUpperBound(_schedule.getEndTime());
 
+        // Extend tick marks
+        for(Axis.TickMark<Number> tickmark : _axis.getTickMarks()) {
+            double x = tickmark.getPosition();
+            gc.strokeLine(x, 0, x, canvasHeight);
+        }
+    }
 }
