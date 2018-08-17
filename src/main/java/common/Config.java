5dde073ea02f571270f9109152114be444ba8044
Nathan Cairns <nathancairns777@gmail.com>
2018-08-17 16:04:22 +1200

    Change primary / secondary colours

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -10,9 +10,9 @@ public class Config {
     public static final int PROCESSORS_DEFAULT = 1;
 
     // UI stuff
-    public static final String UI_PRIMARY_COLOUR = "#8d71ff";
-    public static final String UI_SECONDAY_COLOR = "#ffc671";
+    public static final String UI_PRIMARY_COLOUR = "#b8a2ff";
+    public static final String UI_SECONDAY_COLOR = "#ffdba2";
     public static final String UI_LIGHT_BLACK_COLOUR = "#484848";
-    public static final String UI_GREEN_COLOUR = "#71ffaa";
+    public static final String UI_GREEN_COLOUR = "#a2ffcd";
     public static final String UI_WHITE_COLOUR = "#fafafa";
 }
