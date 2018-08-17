27a478aa4aa4a92cc3a6492231440ea13829695a
Timothy Finucane <timfinucane@outlook.com>
2018-08-17 16:17:49 +1200

    Fix accidental changes

--- src/test/java/AinurVisualiserTest.java
+++ src/test/java/AinurVisualiserTest.java
@@ -7,11 +7,7 @@ public class AinurVisualiserTest {
 
     @Test
     public void testVisualiser() {
-<<<<<<< HEAD
         Ainur.main(new String[]{"data/graphs/Nodes_11_OutTree.dot", "4", "-v"});
-=======
-        Ainur.main(new String[]{"data/graphs/Nodes_11_OutTree.dot", "100", "-v"});
->>>>>>> 7b704f2d0fe2561a6bc14f872b9978db3a8d7b78
     }
 
 }
