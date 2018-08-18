afbcfb9898867323f7ce4d35e02504223e79c6b5
Nathan Cairns <nathancairns777@gmail.com>
2018-08-18 20:57:56 +1200

    Add css colours template

--- src/main/java/visualisation/themes/Theme.java
+++ src/main/java/visualisation/themes/Theme.java
@@ -3,7 +3,8 @@ package visualisation.themes;
 /**
  * This interface is used for themeing Ainur's visualisers.
  * Overwrite each of these methods to return a string hexcode.
- * The getCss method should reference a CSS file with the same scheme
+ * The getCss method should reference a CSS file with the same scheme.
+ * See the ThemeTemplate.txt file in resources/style/colours for more info on this.
  */
 
 public interface Theme {
