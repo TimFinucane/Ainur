3276439098b611d27d49d65d973edacfa76c9e19
Nathan Cairns <nathancairns777@gmail.com>
2018-08-18 15:49:10 +1200

    Add app icon

--- src/main/java/VisualiserWindow.java
+++ src/main/java/VisualiserWindow.java
@@ -1,10 +1,12 @@
 import algorithm.Algorithm;
+import com.sun.javafx.tk.Toolkit;
 import common.graph.Graph;
 import javafx.application.Platform;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -14,6 +16,7 @@ import visualisation.AinurVisualiser;
 public class VisualiserWindow {
     /* Macros */
     private static final String STYLE_SHEET = "/style/Ainur.css";
+    private static final String APP_ICON = "/img/icon.png";
 
     // Window state
     private double _x;
@@ -69,6 +72,7 @@ public class VisualiserWindow {
         _av.getStyleClass().add("ainur-vis");
 
         _scene.getStylesheets().add(getClass().getResource(STYLE_SHEET).toExternalForm());
+        _stage.getIcons().add(new Image(getClass().getResourceAsStream(APP_ICON)));
         primaryStage.show();
 
         _av.run();
